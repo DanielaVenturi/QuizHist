b87a5ebb5d3ba0447d0aa2b4e5306376792e86b5
DanielaVenturi <danielaventuri14@gmail.com>
2023-07-26 14:53:25 -0300

    router

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="nav">
+  <router-link to="/">Home</router-link>
+  <router-link to="/About">About</router-link>
+</div>
+ <router-view/>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/BD.vue
@@ -0,0 +1,321 @@
+const pergunta = [
+{
+    id:1,
+    perg:'O que é economia?',
+    respostas: [
+    {
+       letra: "A",
+       texto: "A economia é um conjunto de práticas de produção, distribuição e consumo de bens.",
+       correta: "yes",
+    },
+    {
+        letra: "B",
+        texto: "É a Regulação dos espaços industriais pelo Estado. ",
+        correta: "no",
+     },
+     {
+        letra: "C",
+        texto: " É a formação de trustes.  ",
+        correta: "no",
+     },
+     {
+        letra: "D",
+        texto: "É o predomínio de pequenas empresas na economia.  ",
+        correta: "no",
+     },
+     {
+        letra: "E",
+        texto: "Economia é a ciência de prever o futuro do mercado financeiro usando uma bola de cristal.  ",
+        correta: "no",
+     },
+     
+    ]
+},
+
+{
+    id:2,
+    perg:'Quando ocorreu a transição do feudalismo para o capitalismo?',
+    respostas: [
+    {
+       letra: "A",
+       texto: "Ocorreu no século X e só terminará por volta do século XVII. ",
+       correta: "no",
+    },
+    {
+        letra: "B",
+        texto: "Ocorreu no século XI e só terminará por volta do século XV. 
+        ",
+        correta: "no",
+     },
+     {
+        letra: "C",
+        texto: "Ocorreu no século XI e só terminará por volta do século XVI. 
+        ",
+        correta: "yes",
+     },
+     {
+        letra: "D",
+        texto: "Ocorreu no século XVI e só terminará por volta do século XXI.  ",
+        correta: "no",
+     },
+     {
+        letra: "E",
+        texto: "Ocorreu no século XVIII e só terminará por volta do século XX.  ",
+        correta: "no",
+     },
+     
+    ]
+},
+    
+
+{
+    id:3,
+    perg:'Assinale o fenômeno que ocorreu na transição do feudalismo para o capitalismo: ',
+    respostas: [
+    {
+       letra: "A",
+       texto: " Ocorreu uma a grande depressão. ",
+       correta: "no",
+    },
+    {
+        letra: "B",
+        texto: "Ocorreu uma grave crise de fome.  ",
+        correta: "yes",
+     },
+     {
+        letra: "C",
+        texto: "Ocorreu uma grave crise dos mercados emergentes.  ",
+        correta: "no",
+     },
+     {
+        letra: "D",
+        texto: "Ocorreu uma grave crise mundial do subprime. ",
+        correta: "no",
+     },
+     {
+        letra: "E",
+        texto: "Ocorreu uma grave pandemia de malária.",
+        correta: "no",
+     },
+     
+    ]
+},
+{
+    id:4,
+    perg:'Qual foi a doença que ocorreu no século XVI ? '
+    respostas: [
+    {
+       letra: "A",
+       texto: "Gripe espanhola . ",
+       correta: "no",
+    },
+    {
+        letra: "B",
+        texto: " Cólera. ",
+        correta: "no",
+     },
+     {
+        letra: "C",
+        texto: " Varíola. ",
+        correta: "no",
+     },
+     {
+        letra: "D",
+        texto: "Peste negra. ",
+        correta: "yes",
+     },
+     {
+        letra: "E",
+        texto: "Tuberculose.",
+        correta: "no",
+     },
+     
+    ]
+},
+{
+    id:5,
+    perg:'Qual foi a guerra que aconteceu por volta do século XVI? '
+    respostas: [
+    {
+       letra: "A",
+       texto: "1° guerra mundial ",
+       correta: "no",
+    },
+    {
+        letra: "B",
+        texto: "Guerra dos três reinos ",
+        correta: "no",
+     },
+     {
+        letra: "C",
+        texto: "Guerra dos cem anos",
+        correta: "yes",
+     },
+     {
+        letra: "D",
+        texto: "2° guerra mundial ",
+        correta: "no",
+     },
+     {
+        letra: "E",
+        texto: " Guerra Fria ",
+        correta: "no",
+     },
+     
+    ]
+},
+{
+    id:6,
+    perg:'Qual foi o material que ficou esgotado durante o século XVI na Europa? '
+    respostas: [
+    {
+       letra: "A",
+       texto: "Metais ",
+       correta: "yes",
+    },
+    {
+        letra: "B",
+        texto: "Algodão ",
+        correta: "no",
+     },
+     {
+        letra: "C",
+        texto: "Produtos agrícolas ",
+        correta: "no",
+     },
+     {
+        letra: "D",
+        texto: "Carne bovina ",
+        correta: "no",
+     },
+     {
+        letra: "E",
+        texto: "Carvao",
+        correta: "no",
+     },
+     
+    ]
+},
+{
+    id:7,
+    perg:'Qual foi o 1° marco histórico da idade moderna? '
+    respostas: [
+    {
+       letra: "A",
+       texto: "Foi a Tomada de Constantinopla pelos Turcos ",
+       correta: "yes",
+    },
+    {
+        letra: "B",
+        texto: "Foi o fim da Guerra do Golfo. ",
+        correta: "no",
+     },
+     {
+        letra: "C",
+        texto: "Foi o Acidente nuclear de Chernobyl. ",
+        correta: "no",
+     },
+     {
+        letra: "D",
+        texto: "Foi a Abolição da escravatura na Mauritânia.",
+        correta: "no",
+     },
+     {
+        letra: "E",
+        texto: "Foi o fim dos atos terroristas de 8 de janeiro.",
+        correta: "no",
+     },
+     
+    ]
+},
+{
+    id:8,
+    perg: 'Quais eram os motivos de que a burguesia queria centralizar seu poder? E o que iria financiar '
+    respostas: [
+    {
+       letra: "A",
+       texto: "Os burgueses queriam deixar o regime, estavam satisfeitos com as mercadorias e não iram financiar nada.",
+       correta: "no",
+    },
+    {
+        letra: "B",
+        texto: "A burguesia ia resolver as coisas sozinhos e iam financiar os pobres.  ",
+        correta: "no",
+     },
+     {
+        letra: "C",
+        texto: "A burguesia estava insatisfeita com a alta circulação de moedas, e que eles dependem dos feudos para o processo de segurança do transporte de mercadorias, e financiou um exército mercenário para os nobres. ",
+        correta: "yes",
+     },
+     {
+        letra: "D",
+        texto: "A burguesia estava satisfeita com a alta circulação de moedas, assim ajudaram os pobres financiando terras e animais.",
+        correta: "no",
+     },
+     {
+        letra: "E",
+        texto: " A burguesia ia resolver as coisas sozinhos, porém não iam financiar os pobres. ",
+        correta: "no",
+     },
+     {
+      id:9,
+      perg: 'O que justifica o processo que a tem de expansão marítima e de domínio do "novo mundo"? '
+      respostas: [
+      {
+         letra: "A",
+         texto: "Eles precisavam de poder de outras terras para lidar com o feudalismo.",
+         correta: "no",
+      },
+      {
+          letra: "B",
+          texto: "Eles precisavam da conquista de novas áreas produtoras agrícolas.",
+          correta: "yes",
+       },
+       {
+          letra: "C",
+          texto: "Eles precisavam de pessoas para evangelizar e fazer dinheiro.",
+          correta: "no",
+       },
+       {
+          letra: "D",
+          texto: "Eles precisavam de mais escravos, assim explorando terras para sequestrá-los.",
+          correta: "no",
+       },
+       {
+          letra: "E",
+          texto: "Eles precisavam de mais minérios. ",
+          correta: "no",
+       },
+       {
+         id:10,
+         perg: 'Qual foi o motivo da crise clerical? Que evento houve por causa dessa crise?'[
+         {
+            letra: "A",
+            texto: "Eles precisavam de poder de outras terras para lidar com o feudalismo.",
+            correta: "no",
+         },
+         {
+             letra: "B",
+             texto: "Eles precisavam da conquista de novas áreas produtoras agrícolas.",
+             correta: "yes",
+          },
+          {
+             letra: "C",
+             texto: "Eles precisavam de pessoas para evangelizar e fazer dinheiro.",
+             correta: "no",
+          },
+          {
+             letra: "D",
+             texto: "Eles precisavam de mais escravos, assim explorando terras para sequestrá-los.",
+             correta: "no",
+          },
+          {
+             letra: "E",
+             texto: "Eles precisavam de mais minérios. ",
+             correta: "no",
+          },
+     
+    ]
+    
+}
+]
