9f1ee4d194e2feca7ba9c07f0f230fa47bf82396
DanielaVenturi <danielaventuri14@gmail.com>
2023-07-26 14:58:25 -0300

    navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+<div class="nav">
+<router-link>
+    <img src="" id="logo">
+</router-link>
+  <router-link to="/">Home</router-link>
+  <router-link to="/About">About</router-link>
+</div>
+</template>
+<style scoped>
+
+</style>
